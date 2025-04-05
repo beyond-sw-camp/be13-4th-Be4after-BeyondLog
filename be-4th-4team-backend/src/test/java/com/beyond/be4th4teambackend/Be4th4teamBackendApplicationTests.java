ec656662fad85d64835a2149518f3f9d90744ae0
OhGlory <a25553683@gmail.com>
2025-04-05 12:43:17 +0900

    [오영광]초기 세팅 디렉터리

---
+++ be-4th-4team-backend/src/test/java/com/beyond/be4th4teambackend/Be4th4teamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.be4th4teambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be4th4teamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
