8a00eb7b353f600b422bceba184ff3b91353e6d9
g00dbyul <skypower4203@gmail.com>
2025-04-14 15:03:56 +0900

    fix: revert notice controller

--- be-4th-4team-backend/src/main/java/com/beyond/be4th4teambackend/notice/controller/NoticeController.java
+++ be-4th-4team-backend/src/main/java/com/beyond/be4th4teambackend/notice/controller/NoticeController.java
@@ -19,44 +19,44 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/v1/notice")
-//@Tag(name = "Notice API", description = "공지 관련 API 목록")
-//public class NoticeController {
-//    private final NoticeService noticeService;
-//
-//    @PostMapping
-//    @Operation(summary = "공지 작성", description =  "공지를 작성합니다.")
-//    public ResponseEntity<NoticeResponseDto> createNotice(@RequestBody CreateAndUpdateNoticeDto createNoticeDto) {
-//        return ResponseEntity.status(HttpStatus.CREATED).body(noticeService.createNotice(createNoticeDto));
-//    }
-//
-//    @GetMapping
-//    @Operation(summary = "공지 조회", description = "공지를 조회합니다.")
-//    public ResponseEntity<List<NoticeResponseDto>> getNotices() {
-//        return ResponseEntity.ok(noticeService.getNoticePosts());
-//    }
-//
-//    @GetMapping("/{noticeId}")
-//    @Operation(summary = "공지 상세 조회", description = "해당 공지를 상세 조회합니다.")
-//    public ResponseEntity<NoticeResponseDto> getNotice(@PathVariable Long noticeId) {
-//        return ResponseEntity.ok(noticeService.getNoticeById(noticeId));
-//    }
-//
-//    @PutMapping("/{noticeId}")
-//    @Operation(summary = "공지 수정", description = "공지를 수정합니다.")
-//    public ResponseEntity<NoticeResponseDto> updateNotice(@PathVariable Long noticeId,
-//                                                          @RequestBody CreateAndUpdateNoticeDto updateNoticeDto) {
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(noticeService.updateNotice(noticeId, updateNoticeDto));
-//    }
-//
-//    @DeleteMapping("/{userId}/{noticeId}")
-//    @Operation(summary = "공지 삭제", description = "공지를 삭제합니다.")
-//    public ResponseEntity<Void> deleteNotice(@PathVariable Long userId,
-//                                             @PathVariable Long noticeId) {
-//        noticeService.deleteNotice(userId, noticeId);
-//        return ResponseEntity.ok().build();
-//    }
-//}
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/notice")
+@Tag(name = "Notice API", description = "공지 관련 API 목록")
+public class NoticeController {
+    private final NoticeService noticeService;
+
+    @PostMapping
+    @Operation(summary = "공지 작성", description =  "공지를 작성합니다.")
+    public ResponseEntity<NoticeResponseDto> createNotice(@RequestBody CreateAndUpdateNoticeDto createNoticeDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(noticeService.createNotice(createNoticeDto));
+    }
+
+    @GetMapping
+    @Operation(summary = "공지 조회", description = "공지를 조회합니다.")
+    public ResponseEntity<List<NoticeResponseDto>> getNotices() {
+        return ResponseEntity.ok(noticeService.getNoticePosts());
+    }
+
+    @GetMapping("/{noticeId}")
+    @Operation(summary = "공지 상세 조회", description = "해당 공지를 상세 조회합니다.")
+    public ResponseEntity<NoticeResponseDto> getNotice(@PathVariable Long noticeId) {
+        return ResponseEntity.ok(noticeService.getNoticeById(noticeId));
+    }
+
+    @PutMapping("/{noticeId}")
+    @Operation(summary = "공지 수정", description = "공지를 수정합니다.")
+    public ResponseEntity<NoticeResponseDto> updateNotice(@PathVariable Long noticeId,
+                                                          @RequestBody CreateAndUpdateNoticeDto updateNoticeDto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(noticeService.updateNotice(noticeId, updateNoticeDto));
+    }
+
+    @DeleteMapping("/{userId}/{noticeId}")
+    @Operation(summary = "공지 삭제", description = "공지를 삭제합니다.")
+    public ResponseEntity<Void> deleteNotice(@PathVariable Long userId,
+                                             @PathVariable Long noticeId) {
+        noticeService.deleteNotice(userId, noticeId);
+        return ResponseEntity.ok().build();
+    }
+}
